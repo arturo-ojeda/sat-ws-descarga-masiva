f9d651f58eeea3668096c9a78a960be72316f8c3
David Ojeda <david.ojeda@david.ojeda>
2024-01-30 15:32:01 -0700

    Add received param as default

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import Fastify from 'fastify'
 import multipart from '@fastify/multipart';
-import { Fiel, HttpsWebClient, FielRequestBuilder, Service, ServiceEndpoints } from '@nodecfdi/sat-ws-descarga-masiva';
+import { Fiel, HttpsWebClient, FielRequestBuilder, Service, ServiceEndpoints, DownloadType } from '@nodecfdi/sat-ws-descarga-masiva';
 import { install } from '@nodecfdi/cfdiutils-common';
 import { DOMParser, XMLSerializer, DOMImplementation } from '@xmldom/xmldom';
 import { QueryParameters, DateTimePeriod } from '@nodecfdi/sat-ws-descarga-masiva';
@@ -33,7 +33,9 @@ fastify.post('/', async function handler(request, reply) {
     // Creación del servicio
     const service = new Service(requestBuilder, webClient, undefined, ServiceEndpoints.retenciones());
     const satRequest = QueryParameters.create(
-        DateTimePeriod.createFromValues(startDate, endDate));
+        DateTimePeriod.createFromValues(startDate, endDate),
+        new DownloadType('received')
+    )
 
     const query = await service.query(satRequest);
 
@@ -46,6 +48,59 @@ fastify.post('/', async function handler(request, reply) {
     reply.code(200).send({ requestId: query.getRequestId() });
 })
 
+// curl -X POST -H "Content-Type: multipart/form-data" -F "cert=@dummy.cer" -F "key=@dummy.key" -F "passphrase=dummy" -F "requestId=281ab537-7e13-4387-a035-6298139ef09a" http://localhost:3000/
+fastify.post('/verify', async function handler(request, reply) {
+    const { certFile, keyFile, passphrase, requestId } = await extractParts(request);
+    const fiel = Fiel.create(certFile, keyFile, passphrase);
+
+    console.info({requestId});
+
+    // Creación del cliente web se usa el cliente incluido en nodeJS.
+    const webClient = new HttpsWebClient();
+
+    // creación del objeto encargado de crear las solicitudes firmadas usando una FIEL
+    const requestBuilder = new FielRequestBuilder(fiel);
+
+    // Creación del servicio
+    const service = new Service(requestBuilder, webClient, undefined, ServiceEndpoints.retenciones());
+    const verify = await service.verify(requestId);
+
+    // revisar que el proceso de verificación fue correcto
+    if (!verify.getStatus().isAccepted()) {
+        console.error(`Fallo al verificar la consulta ${requestId}: ${verify.getStatus().getMessage()}`);
+        reply.code(400).send({ message: verify.getStatus().getMessage() });
+        return;
+    }
+
+    // revisar el progreso de la generación de los paquetes
+    const statusRequest = verify.getStatusRequest();
+    console.info({statusRequest})
+
+    const codeRequest = verify.getCodeRequest();
+    console.info({codeRequest})
+
+    console.info({verify})
+
+    if (statusRequest.isTypeOf('Expired') || statusRequest.isTypeOf('Failure') || statusRequest.isTypeOf('Rejected')) {
+        reply.code(400).send({ message: `La solicitud ${requestId} no se puede completar` });
+        return;
+    }
+    if (statusRequest.isTypeOf('InProgress') || statusRequest.isTypeOf('Accepted')) {
+        reply.code(200).send(`La solicitud ${requestId} se está procesando`);
+        return;
+    }
+    if (statusRequest.isTypeOf('Finished')) {
+        console.info(`La solicitud ${requestId} está lista`);
+    }
+
+    console.info(`Se encontraron ${verify.countPackages()} paquetes`);
+    for (const packageId of verify.getPackageIds()) {
+        console.log(` > ${packageId}`)
+    }
+
+    reply.code(200).send({message: verify.getPackageIds()});
+})
+
 try {
     await fastify.listen({ port: SERVER_PORT })
 } catch (err) {
@@ -55,7 +110,7 @@ try {
 
 async function extractParts(request) {
     const parts = request.parts();
-    let certFile, keyFile, passphrase, startDate, endDate;
+    let certFile, keyFile, passphrase, startDate, endDate, requestId;
 
     for await (const part of parts) {
         if (part.file) {
@@ -71,9 +126,11 @@ async function extractParts(request) {
                 startDate = part.value + ' 00:00:00';
             } else if (part.fieldname === 'endDate') {
                 endDate = part.value + ' 00:00:00';
+            } else if (part.fieldname === 'requestId') {
+                requestId = part.value;
             }
         }
     }
-    return { certFile, keyFile, passphrase, startDate, endDate };
+    return { certFile, keyFile, passphrase, startDate, endDate, requestId };
 }
 
