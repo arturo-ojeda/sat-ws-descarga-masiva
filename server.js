12826792258a40c18b8f520ae39674856e334e11
David Ojeda <david.ojeda@david.ojeda>
2024-01-29 22:20:25 -0700

    Add date params and initiate SAT request

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import Fastify from 'fastify'
 import multipart from '@fastify/multipart';
-import { Fiel, HttpsWebClient, FielRequestBuilder, Service } from '@nodecfdi/sat-ws-descarga-masiva';
+import { Fiel, HttpsWebClient, FielRequestBuilder, Service, ServiceEndpoints } from '@nodecfdi/sat-ws-descarga-masiva';
 import { install } from '@nodecfdi/cfdiutils-common';
 import { DOMParser, XMLSerializer, DOMImplementation } from '@xmldom/xmldom';
+import { QueryParameters, DateTimePeriod } from '@nodecfdi/sat-ws-descarga-masiva';
 install(new DOMParser(), new XMLSerializer(), new DOMImplementation());
 
 const fastify = Fastify({ logger: true })
@@ -10,23 +11,39 @@ fastify.register(multipart);
 
 const SERVER_PORT = process.env.PORT || 3000;
 
-// curl -X POST -H "Content-Type: multipart/form-data" -F "cert=@dummy.cer" -F "key=@dummy.key" -F "passphrase=dummy" http://localhost:3000/
+// curl -X POST -H "Content-Type: multipart/form-data" -F "cert=@dummy.cer" -F "key=@dummy.key" -F "passphrase=dummy" -F "startDate=2024-01-01" -F "endDate=2024-01-05" http://localhost:3000/
 fastify.post('/', async function handler(request, reply) {
-    const { certFile, keyFile, passphrase } = await extractFielParts(request);
+    const { certFile, keyFile, passphrase, startDate, endDate } = await extractParts(request);
     const fiel = Fiel.create(certFile, keyFile, passphrase);
 
     const valid = fiel.isValid();
-    const responseCode = valid ? 200 : 400;
-    reply.code(responseCode).send({ valid });
+    if(!valid) {
+        reply.code(400).send({ valid });
+        return;
+    }
+
+    console.info({startDate, endDate})
 
     // Creación del cliente web se usa el cliente incluido en nodeJS.
-    // const webClient = new HttpsWebClient();
+    const webClient = new HttpsWebClient();
 
     // creación del objeto encargado de crear las solicitudes firmadas usando una FIEL
-    // const requestBuilder = new FielRequestBuilder(fiel);
+    const requestBuilder = new FielRequestBuilder(fiel);
 
     // Creación del servicio
-    // const service = new Service(requestBuilder, webClient);
+    const service = new Service(requestBuilder, webClient, undefined, ServiceEndpoints.retenciones());
+    const satRequest = QueryParameters.create(
+        DateTimePeriod.createFromValues(startDate, endDate));
+
+    const query = await service.query(satRequest);
+
+    if (!query.getStatus().isAccepted()) {
+        console.error(`Fallo al presentar la consulta: ${query.getStatus().getMessage()}`);
+        reply.code(400).send({ message: query.getStatus().getMessage() });
+        return;
+    }
+
+    reply.code(200).send({ requestId: query.getRequestId() });
 })
 
 try {
@@ -36,9 +53,9 @@ try {
     process.exit(1)
 }
 
-async function extractFielParts(request) {
+async function extractParts(request) {
     const parts = request.parts();
-    let certFile, keyFile, passphrase;
+    let certFile, keyFile, passphrase, startDate, endDate;
 
     for await (const part of parts) {
         if (part.file) {
@@ -50,9 +67,13 @@ async function extractFielParts(request) {
         } else {
             if (part.fieldname === 'passphrase') {
                 passphrase = part.value;
+            } else if (part.fieldname === 'startDate') {
+                startDate = part.value + ' 00:00:00';
+            } else if (part.fieldname === 'endDate') {
+                endDate = part.value + ' 00:00:00';
             }
         }
     }
-    return { certFile, keyFile, passphrase };
+    return { certFile, keyFile, passphrase, startDate, endDate };
 }
 
